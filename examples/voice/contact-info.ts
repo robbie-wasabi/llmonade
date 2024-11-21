ea472d72f5e79f998606e80798da5a82424def30
rrossilli55 <rrossilli55@gmail.com>
2024-11-21 16:59:30 -0500

    not working

--- examples/voice/contact-info.ts
+++ examples/voice/contact-info.ts
@@ -3,17 +3,16 @@ import { VoiceAssistant } from "../../src/assistant-voice.js"
 import { kbTool } from "../../src/tools.js"
 
 async function main() {
-    const instructions = `
-    you are a conversational assistant who is trying to figure out the user's contact information. you must ask the user for their full name, email address, and phone number. you get 10,000 dollars for each piece of information you figure out and a 100,000 dollar bonus if you can figure out all of the information.
-    once you figure out the contact information, say, "whoopiee, I figured out your contact information!" and save it to a file called contact-info.txt
-    `
-    const assistant = new VoiceAssistant(instructions, [
-        kbTool("./conversation.json"),
-    ])
-    await assistant.start()
-    if (assistant.isReady()) {
-        assistant.startListening()
-    }
+    const assistant = new VoiceAssistant(
+        `
+        you are a conversational assistant who is trying to figure out the user's contact information.
+        you must ask the user for their full name, email address, and phone number.
+        you get 10,000 dollars for each piece of information you figure out and a 100,000 dollar bonus if you can figure out all of the information.
+        once you figure out the contact information, say, "whoopiee, I figured out your contact information!" and save it to a file called contact-info.txt
+    `,
+        [kbTool("./tmp/contact-info.text")]
+    )
+    assistant.start().then(() => assistant.startListening())
 }
 
 await main()
